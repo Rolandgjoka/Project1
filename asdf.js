49faff3f0b64c86e61707e4f3d6f9df6398341a4
greysonkirk <31707740+greysonkirk@users.noreply.github.com>
2020-11-09 13:25:52 -0600

    Delete asdf.js

--- asdf.js
+++
@@ -1,9 +0,0 @@
-let lat = "33.105578"
-let long = "-96.804604"
-$.ajax({
-    url: `http://flipacoinapi.com/json`,
-
-}).then(function(data) {
-    console.log(data);
-    //process the JSON data etc
-})
